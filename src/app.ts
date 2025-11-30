a2af214eb85bb820e7cf37d971149d6fbdfcc697
jeeva-2311 <angrysmash1123@gmail.com>
2025-11-30 18:46:30 +0530

    Updated

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,12 @@ import routes from "./routes";
 import cors from "cors";
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(express.json());
 app.use("/", routes);
